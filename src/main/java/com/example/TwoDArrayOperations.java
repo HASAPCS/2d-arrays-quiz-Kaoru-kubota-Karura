a4135e4cc70217e4c9c3b7bd94649eb79d077600
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-12-08 12:30:32 +0800

    why do "MAX_VALUE" can work but "MIN_VALUE" not?

--- src/main/java/com/example/TwoDArrayOperations.java
+++ src/main/java/com/example/TwoDArrayOperations.java
@@ -5,18 +5,41 @@ public class TwoDArrayOperations {
     // Exercise 1: Check for a Specific Value in a 2D Array
     public static boolean containsValue(int[][] array, int value) {
         // TODO: Implement the logic to check if the 2D array contains the specified value.
-        return false; // Placeholder return value
+        for (int[] row : array) {
+            for (int element : row) {
+                if (element == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+        // Placeholder return value
     }
 
     // Exercise 2: Calculate the Average of All Elements in a 2D Array
     public static double calculateAverage(int[][] array) {
         // TODO: Implement the logic to calculate the average of all elements in the 2D array.
-        return 0.0; // Placeholder return value
+        int sum = 0;
+        int count = 0;
+        for (int[] row : array) {
+            for (int value : row) {
+                sum += value;
+                count++;
+            }
+        }
+        return (double) sum / count;
+        // Placeholder return value
     }
 
     // Exercise 3: Find the Minimum Value in a 2D Array
     public static int findMinimumValue(int[][] array) {
         // TODO: Implement the logic to find the minimum value in the 2D array.
-        return 0; // Placeholder return value
+        int min = Integer.MAX_VALUE;
+        for (int[] row : array) {
+            for (int value : row) {
+                min = Math.min(min, value);
+            }
+        }
+        return min;
     }
 }
